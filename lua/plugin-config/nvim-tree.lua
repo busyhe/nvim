e7233156788704d8fcd347b12e32c939e0661bb6
busyhe <busyhe@qq.com>
2022-07-16 21:43:42 +0800

    style: update nvim-tree config

--- lua/plugin-config/nvim-tree.lua
+++ lua/plugin-config/nvim-tree.lua
@@ -7,24 +7,21 @@ end
 -- 列表操作快捷键
 local list_keys = require('keybindings').nvimTreeList
 nvim_tree.setup({
-    -- 不显示 git 状态图标
-    git = {
-        enable = false,
-    },
     -- project plugin 需要这样设置
     update_cwd = true,
     update_focused_file = {
         enable = true,
         update_cwd = true,
     },
-    -- 隐藏 .文件 和 node_modules 文件夹
+    -- node_modules 文件夹
     filters = {
+        -- 隐藏 .文件
         dotfiles = true,
-        custom = { 'node_modules' },
+        custom = { 'node_modules'},
     },
     view = {
         -- 宽度
-        width = 40,
+        width = 30,
         -- 也可以 'right'
         side = 'left',
         -- 隐藏根目录
@@ -44,8 +41,6 @@ nvim_tree.setup({
         open_file = {
             -- 首次打开大小适配
             resize_window = true,
-            -- 打开文件时关闭
-            quit_on_open = true,
         },
     },
     -- wsl install -g wsl-open
