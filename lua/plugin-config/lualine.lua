6b12aa4854fa70505d8adc27d3db3156b659a52b
busyhe <busyhe@qq.com>
2022-07-16 22:08:53 +0800

    style: update lualine style

--- lua/plugin-config/lualine.lua
+++ lua/plugin-config/lualine.lua
@@ -7,10 +7,7 @@ end
 
 lualine.setup({
   options = {
-    theme = "nord",
-    component_separators = { left = "|", right = "|" },
-    -- https://github.com/ryanoasis/powerline-extra-symbols
-    section_separators = { left = " ", right = "" },
+    theme = "nord"
   },
   extensions = { "nvim-tree", "toggleterm" },
   sections = {
