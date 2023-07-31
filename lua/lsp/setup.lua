0acfb9b521cec03e171551299de6d8d2ceffbb41
busyhe <busyhe@qq.com>
2023-08-01 02:38:39 +0800

    fix(lua plugin): some error

--- lua/lsp/setup.lua
+++ lua/lsp/setup.lua
@@ -5,7 +5,7 @@ local lsp_installer = require('nvim-lsp-installer')
 -- key 必须为下列网址列出的名称
 -- https://github.com/williamboman/nvim-lsp-installer#available-lsps
 local servers = {
-    sumneko_lua = require('lsp.config.lua'), -- lua/lsp/config/lua.lua
+    lua_ls = require('lsp.config.lua'),
     html = require('lsp.config.html'),
     cssls = require('lsp.config.css'),
     emmet_ls = require('lsp.config.emmet'),
