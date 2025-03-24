ebf51e0acbc4535277eac629be5ac590b6708bc4
busyhe <busyhe@qq.com>
2025-03-24 14:28:59 +0800

    feat: update indent-blankline plugin version

--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -49,8 +49,8 @@ local function save_profiles(threshold)
 end
 
 time([[Luarocks path setup]], true)
-local package_path_str = "/Users/busyhe/.cache/nvim/packer_hererocks/2.1.0-beta3/share/lua/5.1/?.lua;/Users/busyhe/.cache/nvim/packer_hererocks/2.1.0-beta3/share/lua/5.1/?/init.lua;/Users/busyhe/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/luarocks/rocks-5.1/?.lua;/Users/busyhe/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/luarocks/rocks-5.1/?/init.lua"
-local install_cpath_pattern = "/Users/busyhe/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/lua/5.1/?.so"
+local package_path_str = "/Users/busyhe/.cache/nvim/packer_hererocks/2.1.1741730670/share/lua/5.1/?.lua;/Users/busyhe/.cache/nvim/packer_hererocks/2.1.1741730670/share/lua/5.1/?/init.lua;/Users/busyhe/.cache/nvim/packer_hererocks/2.1.1741730670/lib/luarocks/rocks-5.1/?.lua;/Users/busyhe/.cache/nvim/packer_hererocks/2.1.1741730670/lib/luarocks/rocks-5.1/?/init.lua"
+local install_cpath_pattern = "/Users/busyhe/.cache/nvim/packer_hererocks/2.1.1741730670/lib/lua/5.1/?.so"
 if not string.find(package.path, package_path_str, 1, true) then
   package.path = package.path .. ';' .. package_path_str
 end
@@ -104,11 +104,11 @@ _G.packer_plugins = {
     path = "/Users/busyhe/.local/share/nvim/site/pack/packer/start/cmp-vsnip",
     url = "https://github.com/hrsh7th/cmp-vsnip"
   },
-  ["copilot.vim"] = {
-    loaded = true,
-    path = "/Users/busyhe/.local/share/nvim/site/pack/packer/start/copilot.vim",
-    url = "https://github.com/github/copilot.vim"
-  },
+  -- ["copilot.vim"] = {
+  --   loaded = true,
+  --   path = "/Users/busyhe/.local/share/nvim/site/pack/packer/start/copilot.vim",
+  --   url = "https://github.com/github/copilot.vim"
+  -- },
   ["dashboard-nvim"] = {
     loaded = true,
     path = "/Users/busyhe/.local/share/nvim/site/pack/packer/start/dashboard-nvim",
